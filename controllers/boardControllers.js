1fd79b577762b4656259f96d801e6ac81ef8fc60
Giorgi Gurgenindze <giorgigurgenidze133@gmail.com>
2024-05-12 18:21:04 +0400

    created: -ID route to get a single board, -Post route for creating a new board

--- controllers/boardControllers.js
+++ controllers/boardControllers.js
@@ -18,3 +18,38 @@ exports.getAllBoards = async (req, res) => {
   }
 };
 
+exports.getBoard = async (req, res) => {
+  try {
+    const board = await Board.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        board,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "failed",
+      message: err.message,
+    });
+  }
+};
+
+exports.createBoard = async (req, res) => {
+  try {
+    const board = await Board.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        board,y
+      },
+    });
+  } catch (err) {
+    res.status(401).json({
+      status: "failed",
+      message: err.message,
+    });
+  }
+};
